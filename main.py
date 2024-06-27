87a1e7dc072f0bcd8f804ea67e810f11816d7d10
abobus <dmytro@kali>
2024-06-27 22:13:32 +0300

    alpha 1

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 #branch1
-aboba = input()
-notabobaabiba = 1
+class User:
+    def __init__(self, user_id, nickname):
+        self.user_id = user_id
+        self.nickname = nickname
+    class Portfolio:
+        def __init__(self, total_amount, ):
+            self.total_amount = total_amount
+
+
+
+class Company:
+    def __init__(self, name, current_stock, starting_stock, available_stock):
+        self.name = name
+        self.current_stock = current_stock
+        self.starting_stock = starting_stock
+        self.available_stock = available_stock
